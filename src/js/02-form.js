f467a083158145588ee11d63cc625c386f5565d6
gizemcoban <gizem.coban.r@gmail.com>
2024-09-20 21:19:43 +0300

    form fixed

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,13 +1,43 @@
 const form = document.querySelector("#feedback-form");
 
+document.addEventListener("DOMContentLoaded", function() {
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formData) {
+    form.elements['email'].value = formData.email;
+    form.elements['message'].value = formData.message;
+  }
+  listenForm();
+  submitForm();
+
+});
 export function listenForm(){
+
+  if(localStorage.getItem("feedback-form-state")){
+    const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    form.elements['email'].value = savedData.email || ''; // Eğer email yoksa boş bırak
+    form.elements['message'].value = savedData.message || ''; // Eğer message yoksa boş bırak
+  }
+
   form.addEventListener("input",(event)=>{
+    const formData = {
+      email: form.elements['email'].value.trim(),
+      message: form.elements['message'].value.trim()
+    };
+
+
     const target = event.target;
     if(target.name === 'email'){
-      console.log(`Email: ${target.value}`);
+      const email=target.value;
+      console.log(`Email: ${formData.email}`);
+
     }else if(target.name==='message'){
-      console.log(`Message: ${target.value}`);
+      const message=target.value;
+      console.log(`Message: ${formData.message}`);
+
     }
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    console.log(`local storage Email: ${formData.email}  Message: ${formData.message}`);
+
   });
 };
 
@@ -15,17 +45,23 @@ export function submitForm(){
   form.addEventListener("submit",(event)=>{
     event.preventDefault();
     const email = form.elements['email'].value.trim();
-    console.log(`Email: ${email}`);
     const message = form.elements['message'].value.trim();
-    console.log(`Message: ${message}`);
-    const objForm = {email:email,message:message}
-    localStorage.setItem("feedback-form-state",JSON.stringify(objForm));
-    console.log(`local storage: ${localStorage.getItem("feedback-form-state")}`);
+    if( email !== "" && message !== ""){
+
+      console.log(`Email: ${email}`);
+      console.log(`Message: ${message}`);
+      const objForm = {email:email,message:message}
+      localStorage.setItem("feedback-form-state",JSON.stringify(objForm));
+      console.log(`local storage: ${localStorage.getItem("feedback-form-state")}`);
+
+      form.reset();
+
+      localStorage.removeItem("feedback-form-state");
+  
+    }else{
+      alert("Please enter both values.");
+    }
 
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    listenForm();
-    submitForm();
-  });
